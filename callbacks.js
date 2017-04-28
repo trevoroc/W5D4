30fe55e1cd54b2dbbc02b3620a13cd293c60c9c5
Trevor O'Connor <trevoroc22@gmail.com>
2017-04-27 19:37:11 -0700

    Finish clock

---
+++ callbacks.js
@@ -0,0 +1,23 @@
+class Clock {
+  constructor() {
+    let date = new Date();
+    this.hours = date.getHours();
+    this.minutes = date.getMinutes();
+    this.seconds = date.getSeconds();
+
+    this.printTime();
+    setInterval(this._tick.bind(this), 1000);
+  }
+
+  printTime() {
+    console.log(`${this.hours}:${this.minutes}:${this.seconds}`);
+  }
+
+  _tick() {
+    this.seconds++;
+    this.seconds %= 60;
+    this.printTime();
+  }
+}
+
+const clock = new Clock();
