7fcd72e8d678dad758ae7c4fc6002e511a75a4df
Trevor O'Connor <trevoroc22@gmail.com>
2017-04-27 20:18:37 -0700

    Finish absurdBubbleSort()

---
+++ intro-to-callbacks/absurdBubbleSort.js
@@ -0,0 +1,48 @@
+const readline = require("readline");
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function askIfGreaterThan(el1, el2, callback) {
+  reader.question(`Is ${el1} greater than ${el2}?`, (res) => {
+    callback((res === "yes") ? true : false);
+  });
+}
+
+function innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop) {
+  if (i < arr.length - 1) {
+    
+    askIfGreaterThan(arr[i], arr[i + 1], (isGreaterThan) => {
+      if (isGreaterThan) {
+        let swapVar = arr[i + 1];
+        arr[i + 1] = arr[i];
+        arr[i] = swapVar;
+        madeAnySwaps = true;
+      }
+
+      innerBubbleSortLoop(arr, i + 1, madeAnySwaps, outerBubbleSortLoop);
+    });
+  } else {
+    outerBubbleSortLoop(madeAnySwaps);
+  }
+}
+
+function absurdBubbleSort(arr, sortCompletionCallback) {
+
+  function outerBubbleSortLoop(madeAnySwaps) {
+    if (madeAnySwaps) {
+      innerBubbleSortLoop(arr, 0, false, outerBubbleSortLoop);
+    } else {
+      sortCompletionCallback(arr);
+    }
+  }
+
+  outerBubbleSortLoop(true);
+}
+
+absurdBubbleSort([3, 2, 1], function (arr) {
+  console.log(`Sorted array: ${JSON.stringify(arr)}`);
+  reader.close();
+});
